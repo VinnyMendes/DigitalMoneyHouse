d3b4a8bd5789be33697ac3ac0cece9e481776677
pdrmenezes <pdrmenezes1@gmail.com>
2023-05-17 16:53:42 -0300

    feat:responsividade página signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@
 import { DefaultButton } from "@/components/Button";
 import { FieldInputController } from "@/components/FieldInput/FieldInputController";
 import { Template } from "@/components/Template";
-import { Text, Flex, VStack, useToast, HStack } from "@chakra-ui/react";
+import { Text, Flex, VStack, useToast, HStack, Stack } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { signupSchema, signupZodInfer } from "./schema";
@@ -62,37 +62,45 @@ export default function SignupPage() {
         justifyContent="center"
         p={{ base: "45px", sm: "unset" }}
       >
-        <VStack spacing={"20px"} maxW={"360px"} w="100%">
-          <FieldInputController placeholder="Nome*" name="firstName" type="text" control={control} error={errors.firstName} />
-          {errors.firstName && <Text color={"red"}>{errors.firstName.message}</Text>}
-          <FieldInputController placeholder="Sobrenome*" name="lastName" type="text" control={control} error={errors.lastName} />
-          {errors.lastName && <Text color={"red"}>{errors.lastName.message}</Text>}
-          <FieldInputMaskController mask={"999.999.999-99"} placeholder="CPF*" name="dni" type="text" control={control} error={errors.dni} />
-          {errors.dni && <Text color={"red"}>{errors.dni.message}</Text>}
-          <FieldInputController placeholder="E-mail*" name="email" type="email" control={control} error={errors.email} />
-          {errors.email && <Text color={"red"}>{errors.email.message}</Text>}
+        <VStack spacing={"20px"} maxW={{ base: "360px", md: "720" }} w="100%">
+          <Stack spacing={"20px"} w="100%" direction={{ base: "column", md: "row" }}>
+            <FieldInputController placeholder="Nome*" name="firstName" type="text" control={control} error={errors.firstName} />
+            {errors.firstName && <Text color={"red"}>{errors.firstName.message}</Text>}
+            <FieldInputController placeholder="Sobrenome*" name="lastName" type="text" control={control} error={errors.lastName} />
+            {errors.lastName && <Text color={"red"}>{errors.lastName.message}</Text>}
+          </Stack>
+          <Stack spacing={"20px"} w="100%" direction={{ base: "column", md: "row" }}>
+            <FieldInputMaskController mask={"999.999.999-99"} placeholder="CPF*" name="dni" type="text" control={control} error={errors.dni} />
+            {errors.dni && <Text color={"red"}>{errors.dni.message}</Text>}
+            <FieldInputController placeholder="E-mail*" name="email" type="email" control={control} error={errors.email} />
+            {errors.email && <Text color={"red"}>{errors.email.message}</Text>}
+          </Stack>
           <Text color="#FFFF" fontSize="14px">
             Use entre 6 e 20 caracteres (deve conter pelo menos 1 caractere especial, uma letra maiúscula e um número)
           </Text>
-          <FieldInputController placeholder="Senha*" control={control} name="password" type="password" error={errors.password} />
-          <FieldInputController
-            placeholder="Confirmar senha*"
-            name="confirmPassword"
-            type="password"
-            control={control}
-            error={errors.confirmPassword}
-          />
-          {errors.confirmPassword && <Text color={"red"}>{errors.confirmPassword.message}</Text>}
-          <FieldInputMaskController
-            mask={"(99) 99999-9999"}
-            placeholder="Telefone*"
-            name="phone"
-            type="text"
-            control={control}
-            error={errors.phone}
-          />
-          {errors.phone && <Text color={"red"}>{errors.phone.message}</Text>}
-          <DefaultButton variant="primary" label="Criar conta" isLoading={isSubmitting} type="submit" />
+          <Stack spacing={"20px"} w="100%" direction={{ base: "column", md: "row" }}>
+            <FieldInputController placeholder="Senha*" control={control} name="password" type="password" error={errors.password} />
+            <FieldInputController
+              placeholder="Confirmar senha*"
+              name="confirmPassword"
+              type="password"
+              control={control}
+              error={errors.confirmPassword}
+            />
+            {errors.confirmPassword && <Text color={"red"}>{errors.confirmPassword.message}</Text>}
+          </Stack>
+          <Stack spacing={"20px"} w="100%" direction={{ base: "column", md: "row" }}>
+            <FieldInputMaskController
+              mask={"(99) 99999-9999"}
+              placeholder="Telefone*"
+              name="phone"
+              type="text"
+              control={control}
+              error={errors.phone}
+            />
+            {errors.phone && <Text color={"red"}>{errors.phone.message}</Text>}
+            <DefaultButton variant="primary" label="Criar conta" isLoading={isSubmitting} type="submit" />
+          </Stack>
         </VStack>
       </Flex>
     </Template>
