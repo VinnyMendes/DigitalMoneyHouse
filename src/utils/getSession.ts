21bbdc61db0c72b8ce5007469da4dba51306ed19
unknown <vinypotter.vm@gmail.com>
2023-05-31 19:58:58 -0300

    sera?

--- src/utils/getSession.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-import { getSession } from "next-auth/react";
-
-export const getSessionClient = async () => {
-  const session = await getSession();
-  const user = session?.user;
-  return user;
-};
